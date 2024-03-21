0c76bc6aebbfa2cc145e101438d35b57f389d1f0
prgrmz07 <prgrmz07@163.com>
2024-03-22 00:17:37 +0800

    草稿中

--- frida-trace.ts
+++ frida-trace.ts
@@ -1,24 +1,34 @@
 //from  https://gitee.com/x619920921/frida-js/raw/main/frida-trace.js
 
-    
-    let moduleMap = new ModuleMap();
-    let base_size = moduleMap.find(addr).size;
+import assert from "assert";
 
-            this.tid = Process.getCurrentThreadId();
-            Stalker.follow(this.tid, {
+    //{ TODO 请用正确的值
+    const base_addr:NativePointer =  new NativePointer(0);
+    const addr:NativePointer=new NativePointer(0);
+    //}
+
+    const moduleMap:ModuleMap = new ModuleMap();
+    const _module:Module|null=moduleMap.find(addr);
+    assert(_module !=null)
+    const base_size:number = _module.size;
+
+            const threadId:ThreadId = Process.getCurrentThreadId();
+            Stalker.follow(threadId, {
                 events: {
                     call: true
                 },
-                onReceive: function(events) {
-                    let allEvents = Stalker.parse(events);
+                onReceive: function(events:ArrayBuffer) {
+                    const allEvents:StalkerEventFull[] = (Stalker.parse(events) as StalkerEventFull[]);
                     let first_depth = 0;
                     let is_first = true;
                     for (let i = 0; i < allEvents.length; i++) {
+                        const evt=allEvents[i]; 
                         // 调用的流程, location是哪里发生的调用, target是调用到了哪里
-                        if (allEvents[i][0] === "call") {
-                            let location = allEvents[i][1]; // 调用地址
-                            let target = allEvents[i][2];   // 目标地址
-                            let depth = allEvents[i][3];    // depth
+                        if (evt[0] === "call") {
+                            const scEvt:StalkerCallEventFull=(evt as StalkerCallEventFull)
+                            let location: NativePointer  = (scEvt[1] as NativePointer); // 调用地址
+                            let target: NativePointer = (scEvt[2] as NativePointer);   // 目标地址
+                            let depth = scEvt[3];    // depth
                             let description = '';
                             let space_num = '';
                             if (target.compare(base_addr) >= 0 && target.compare(base_addr.add(base_size)) < 0) {
