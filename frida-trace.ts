cffa48c4cd227561fb1160b714fb988da6aeb56b
prgrmz07 <prgrmz07@163.com>
2024-03-22 11:16:48 +0800

    最小

--- frida-trace.ts
+++ frida-trace.ts
@@ -2,18 +2,6 @@
 
 function followFunc(){
 
-    //{ TODO 请用正确的值
-    const base_addr:NativePointer =  new NativePointer(0);
-    const addr:NativePointer=new NativePointer(0);
-    //}
-
-    const moduleMap:ModuleMap = new ModuleMap();
-    const _module:Module|null=moduleMap.find(addr);
-    if(_module ==null){
-        return
-    }
-    const base_size:number = _module.size;
-
     const threadId:ThreadId = Process.getCurrentThreadId();
 Stalker.follow(threadId, {
     events: {
@@ -21,33 +9,8 @@ Stalker.follow(threadId, {
     },
     onReceive: function(events:ArrayBuffer) {
         const eventLs: StalkerEventFull[] | StalkerEventBare[] = Stalker.parse(events);
-        let first_depth = 0;
-        let is_first = true;
         for (let i = 0; i < eventLs.length; i++) {
             const evt: StalkerEventFull | StalkerEventBare=eventLs[i]; 
-            // 调用的流程, location是哪里发生的调用, target是调用到了哪里
-            if (evt[0] === "call") {
-                const scEvt:StalkerCallEventFull=(evt as StalkerCallEventFull)
-                let location: NativePointer  = (scEvt[1] as NativePointer); // 调用地址
-                let target: NativePointer = (scEvt[2] as NativePointer);   // 目标地址
-                let depth = scEvt[3];    // depth
-                let description = '';
-                let space_num = '';
-                if (target.compare(base_addr) >= 0 && target.compare(base_addr.add(base_size)) < 0) {
-                    if (is_first) {
-                        is_first = false;
-                        first_depth = depth;
-                    }
-                    let location_description = ' [' + location.sub(base_addr) + '] ';
-                    let target_description = ' [' + location.sub(base_addr) + ']';
-                    let length = (depth - first_depth);
-                    for (let j = 0; j < length; j++) {
-                        space_num = space_num + ' ';
-                    }
-                    description = space_num + target_description + '(' + location_description + ')' + ' -- ' + length;
-                    console.log(description); 
-                } 
-            }
         }
     }
 })
