d4bd7fb38e070865929032c2b5dee6d6f61712da
prgrmz07 <prgrmz07@163.com>
2024-03-22 19:13:41 +0800

    调试信息中的函数详情

--- frida-trace.js
+++ frida-trace.js
@@ -1,14 +1,18 @@
 📦
-381 /frida-trace.js.map
-357 /frida-trace.js
+515 /frida-trace.js.map
+475 /frida-trace.js
 ✄
-{"version":3,"file":"frida-trace.js","sourceRoot":"/fridaAnlzAp/frida_js/","sources":["frida-trace.ts"],"names":[],"mappings":"AACA,SAAS,QAAQ;IAEb,yEAAyE;IAEzE,IAAI,GAAG,GAAC,OAAO,CAAC,gBAAgB,CAAC,eAAe,CAAC,CAAC;IAClD,MAAM,iBAAiB,GAAiB,WAAW,CAAC,qBAAqB,CAAC,GAAG,CAAC,CAAA;IAC9E,OAAO,CAAC,GAAG,CAAC,aAAa,iBAAiB,CAAC,MAAM,EAAE,CAAC,CAAA;IACpD,kBAAkB;AAEtB,CAAC;AAKD,QAAQ,EAAE,CAAA"}
+{"version":3,"file":"frida-trace.js","sourceRoot":"/fridaAnlzAp/frida_js/","sources":["frida-trace.ts"],"names":[],"mappings":"AACA,SAAS,QAAQ;IAEb,yEAAyE;IAEzE,MAAM,GAAG,GAAC,OAAO,CAAC,gBAAgB,CAAC,eAAe,CAAC,CAAC;IACpD,MAAM,UAAU,GAAiB,WAAW,CAAC,qBAAqB,CAAC,GAAG,CAAC,CAAA;IACvE,OAAO,CAAC,GAAG,CAAC,aAAa,UAAU,CAAC,MAAM,EAAE,CAAC,CAAA;IAC7C,kBAAkB;IAElB,KAAK,IAAI,QAAQ,IAAI,UAAU,EAAE;QAC7B,MAAM,KAAK,GAAa,WAAW,CAAC,WAAW,CAAC,QAAQ,CAAC,CAAC;QAC1D,OAAO,CAAC,GAAG,CAAC,KAAK,CAAC,CAAC;KACtB;AAGL,CAAC;AAKD,QAAQ,EAAE,CAAA"}
 ✄
 function deveFunc() {
     // Process.enumerateModules().forEach(m=>console.log(`module=${m.name}`))
-    var Mod = Process.findModuleByName("libtorch.so.1");
-    const funcLsInDebugSymb = DebugSymbol.findFunctionsMatching("*");
-    console.log(`调试信心中函数个数=${funcLsInDebugSymb.length}`);
+    const Mod = Process.findModuleByName("libtorch.so.1");
+    const funcAddrLs = DebugSymbol.findFunctionsMatching("*");
+    console.log(`调试信心中函数个数=${funcAddrLs.length}`);
     //调试信心中函数个数=289146
+    for (let funcAddr of funcAddrLs) {
+        const funcK = DebugSymbol.fromAddress(funcAddr);
+        console.log(funcK);
+    }
 }
 deveFunc();
--- frida-trace.ts
+++ frida-trace.ts
@@ -3,10 +3,16 @@ function deveFunc(){
 
     // Process.enumerateModules().forEach(m=>console.log(`module=${m.name}`))
 
-    var Mod=Process.findModuleByName("libtorch.so.1");
-    const funcLsInDebugSymb:NativePointer[]=DebugSymbol.findFunctionsMatching("*")
-    console.log(`调试信心中函数个数=${funcLsInDebugSymb.length}`)
+    const Mod=Process.findModuleByName("libtorch.so.1");
+    const funcAddrLs:NativePointer[]=DebugSymbol.findFunctionsMatching("*")
+    console.log(`调试信心中函数个数=${funcAddrLs.length}`)
     //调试信心中函数个数=289146
+
+    for (let funcAddr of funcAddrLs) {
+        const funcK:DebugSymbol=DebugSymbol.fromAddress(funcAddr);
+        console.log(funcK);
+    }
+
 
 }
 
