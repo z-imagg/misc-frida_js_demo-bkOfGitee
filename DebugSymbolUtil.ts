38996284e9e789ca5e3add5d2050c3ae17738aa2
prgrmz07 <prgrmz07@163.com>
2024-03-23 16:54:43 +0800

    草稿

--- DebugSymbolUtil.ts
+++ DebugSymbolUtil.ts
@@ -1,6 +1,7 @@
 ////frida-trace初始化js
 //函数符号表格 全局变量
 const gFnSymTab:Map<NativePointer,DebugSymbol> = new Map();
+let gFnCallId:number = 0;
 //填充函数符号表格
 function findFnDbgSym(fnAdr:NativePointer){
       if(gFnSymTab.has(fnAdr)){
@@ -25,13 +26,15 @@ function findFnDbgSym(fnAdr:NativePointer){
 
 function fridaTraceJsOnEnterBusz(thiz:InvocationContext, log:any, args:any[], state:any){
   // log==console.log
-
   var fnSym = findFnDbgSym(thiz.context.pc)
+  console.log(JSON.stringify(fnSym))
+  gFnCallId++;
+  thiz.fnCallId=gFnCallId;
   //  被frida-trace工具生成的.js函数中的onEnter调用的函数中 可以使用 console.log
-  console.log(`源文件名=${fnSym?.fileName}`)
+  console.log(`OnEnter_fnCallId=${thiz.fnCallId}`)
 
 }
 function fridaTraceJsOnLeaveBusz(thiz:InvocationContext, log:any, retval:any, state:any){
   // log==console.log
-
+  console.log(`OnLeave_fnCallId=${thiz.fnCallId}`)
 }
