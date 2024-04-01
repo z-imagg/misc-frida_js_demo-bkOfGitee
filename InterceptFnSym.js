9ea8bf82b9f37d35f6157f3444d2c4b1259fe5be
prgrmz07 <prgrmz07@163.com>
2024-04-01 21:58:45 +0800

    【解决报错】 【Failed to load script: the connection is closed】；【新报错】【在拦截libc.so.6 pthread_getschedparam时抛出异常说进程已终止并停在frida终端】
    又有新错误
    
    
    在拦截libc.so.6 pthread_getschedparam时抛出异常说进程已终止并停在frida终端
    
    ##Interceptor.attach fnAdr=0x7ffff20962c0;  pthread_getschedparam, 0x7ffff20962c0, libc.so.6, , 0 
    Spawned `/fridaAnlzAp/torch-cpp/v1.0.0/simple_nn.elf`. Resuming main thread!
    Process terminated
    [Local::simple_nn.elf ]-> Exception in thread Thread-1 (_run):
    Traceback (most recent call last):
      File "/app/Miniconda3-py310_22.11.1-1/lib/python3.10/threading.py", line 1016, in _bootstrap_inner
        self.run()
      File "/app/Miniconda3-py310_22.11.1-1/lib/python3.10/threading.py", line 953, in run
        self._target(*self._args, **self._kwargs)
      File "/app/Miniconda3-py310_22.11.1-1/lib/python3.10/site-packages/frida_tools/reactor.py", line 70, in _run
        work()
      File "/app/Miniconda3-py310_22.11.1-1/lib/python3.10/site-packages/frida_tools/_repl_magic.py", line 34, in <lambda>
        repl._reactor.schedule(lambda: repl._resume())
      File "/app/Miniconda3-py310_22.11.1-1/lib/python3.10/site-packages/frida_tools/application.py", line 477, in _resume
        self._device.resume(self._spawned_pid)
      File "/app/Miniconda3-py310_22.11.1-1/lib/python3.10/site-packages/frida/core.py", line 76, in wrapper
        return f(*args, **kwargs)
      File "/app/Miniconda3-py310_22.11.1-1/lib/python3.10/site-packages/frida/core.py", line 776, in resume
        self._impl.resume(self._pid_of(target))
    frida.NotSupportedError: process not found
    
    
    [Local::simple_nn.elf ]->

--- InterceptFnSym.js
+++ InterceptFnSym.js
@@ -148,4 +148,16 @@ function _main_() {
         });
     }
 }
-_main_();
+/**
+frida 运行报超时错误 "Failed to load script: timeout was reached" 解决
+frida 运行报超时错误 "Failed to load script: the connection is closed" 解决
+
+错误的解决办法： 命令行加选项timeout  'frida --timeout 0或-1或很大的数 --file ... '
+
+正确的解决办法是，像下面这样  用 函数setTimeout(... , 0) 包裹 业务代码
+ */
+// frida  https://github.com/frida/frida/issues/113#issuecomment-187134331
+setTimeout(function () {
+    //业务代码
+    _main_();
+}, 0);
