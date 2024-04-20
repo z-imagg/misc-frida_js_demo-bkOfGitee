e7cb7eb2daf1086006673fe35d53715672aa4bdb
prgrmz07 <prgrmz07@163.com>
2024-04-20 11:20:19 +0800

    先构建 若构建失败 再 `npm install` 并 构建， 理由是`npm install` 经常很卡

--- rebuild_ts.sh
+++ rebuild_ts.sh
@@ -7,10 +7,19 @@
 f=$(readlink -f ${BASH_SOURCE[0]})  ; d=$(dirname $f)
 cd $d
 
-#安装依赖
-npm install
 
+
+function build_proj(){
 npx frida-compile  InterceptFnSym.ts --no-source-maps --output InterceptFnSym.js  && \
 #删除frida-compile生成的 js文件开头 乱七八糟的 几行
 sed -i '1,/frida-trace初始化js/d' InterceptFnSym.js
+}
 
+
+#先构建
+build_proj || \
+#若构建失败 再 安装依赖 并 构建
+( npm install && build_proj )
+
+
+#理由是 npm install 经常很卡
