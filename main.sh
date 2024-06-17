167b4d303af73bd020dd9677419518f7091452bf
prgrmz07 <prgrmz07@163.com>
2024-06-17 19:29:25 +0800

    DEMO3

--- main.sh
+++ main.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+
+cd /fridaAnlzAp/frida_js_demo/
 
 ###  临时关闭Linux的ASLR(地址空间随机化) ， 否则 x.so 中的函数地址 每次都不同
 # https://blog.csdn.net/counsellor/article/details/81543197
@@ -6,7 +9,8 @@ echo 0 |sudo tee  /proc/sys/kernel/randomize_va_space
 
 ### frida命令bash补全脚本生成
 set +x ; source /app/Miniconda3-py310_22.11.1-1/bin/activate ; set -x
-pip install frida==16.0.7 frida-tools==12.0.4
+pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
+pip install -r requirements.txt
 helpTxt2bashComplete.py --progFile frida
 source bash-complete--frida.sh
 echo "#frida --<tab><tab> 可获得补全"
@@ -16,7 +20,11 @@ gcc -c -g1 -O0 app.c -o app.obj
 gcc app.obj -o app.elf
 ./app.elf argv1 argv2
 
+bash ./rebuild_ts.sh
 
 # 查找编译产物中的函数
 objdump --syms app.elf | grep fun
 # 0000000000001149 g     F .text  0000000000000055              func01_return_int
+
+
+frida  --load ./InterceptFnSym.js    --output app.log    --file ./app.elf
--- rebuild_ts.sh
+++ rebuild_ts.sh
@@ -1,11 +1,10 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 #【描述】 重新编译 ts 为 js 
 #【备注】 并删除frida-compile再js文件开头添加的乱七八糟的几行
 
 #去此脚本所在目录
-f=$(readlink -f ${BASH_SOURCE[0]})  ; d=$(dirname $f)
-cd $d
+cd /fridaAnlzAp/frida_js_demo/
 
 
 
