78c9e9d29f51cbaaa27aaa3aecae4c962be681db
prgrmz07 <prgrmz07@163.com>
2024-06-18 11:01:12 +0800

    frida中的alloc内存会被垃圾回收, 理论上不必作为全局量，但写作全局变量更合理？

--- InterceptFnSym.js
+++ InterceptFnSym.js
@@ -98,6 +98,13 @@ const M_ascii = 'M'.charCodeAt(0);
 // int func04_retVoid_outArgCharBuffer(double _doubleNum, long _longInt, char* outArg_CharBuffer)
 //持有本地函数
 let nativeFn__func04_retVoid_outArgCharBuffer;
+const _Concat_Limit = 50;
+const _Concat_CntTop = 4;
+const _Buffer_Limit = _Concat_CntTop * _Concat_Limit;
+// frida中的alloc内存会被垃圾回收, 理论上不必作为全局量，但写作全局变量更合理？
+const outArg_CharBuffer = Memory.alloc(_Buffer_Limit);
+const _OK = 0;
+const NULL_num = NULL.toInt32();
 /**  OnLeave ，函数离开
  */
 function OnFnLeaveBusz(thiz, retval) {
@@ -114,7 +121,7 @@ function OnFnLeaveBusz(thiz, retval) {
         console.log(`[nativeFn__func01_return_int],ret_int=[${ret_int}]`);
     }
     //调用本地函数 func03_retVoid_outArgPtrStructUser
-    if (nativeFn__func03_retVoid_outArgPtrStructUser.toInt32() != NULL.toInt32()) {
+    if (nativeFn__func03_retVoid_outArgPtrStructUser.toInt32() != NULL_num) {
         const outArg_ptrStructUsr = Memory.alloc(C_Lang__sizeof_structTUser);
         //指针参数outArg_ptrStructUsr携带返回结构体
         nativeFn__func03_retVoid_outArgPtrStructUser.call(null, 4, M_ascii, outArg_ptrStructUsr);
@@ -123,26 +130,36 @@ function OnFnLeaveBusz(thiz, retval) {
         console.log(`[outArg_ptrStructUsr],{userId=${retStructTUser.userId},salary=${retStructTUser.salary}, sum=${retStructTUser.sum} }`);
         // {userId=204,salary=3000.10009765625, sum=-123 }, 结果正确
     }
-    const _Concat_Limit = 50;
-    const _Concat_CntTop = 4;
-    const _Buffer_Limit = _Concat_CntTop * _Concat_Limit;
-    const _OK = 0;
     //调用本地函数 func04_retVoid_outArgCharBuffer
-    if (nativeFn__func04_retVoid_outArgCharBuffer.toInt32() != NULL.toInt32()) {
-        const outArg_CharBuffer = Memory.alloc(_Buffer_Limit);
+    if (nativeFn__func04_retVoid_outArgCharBuffer.toInt32() != NULL_num) {
         //指针参数outArg_CharBuffer携带返回字符串
         //     int func04_ret_code=func04_retVoid_outArgCharBuffer(4.0, 17, CharBuffer);
-        const func04_ret_code = nativeFn__func04_retVoid_outArgCharBuffer(4.0, 17, outArg_CharBuffer);
-        console.log(`func04_ret_code=${func04_ret_code}`);
-        // func04_ret_code=0
+        //第1次调用
+        let func04_ret_code = nativeFn__func04_retVoid_outArgCharBuffer(4.0, 17, outArg_CharBuffer);
+        console.log(`[调用1] func04_ret_code=${func04_ret_code}`);
+        // [调用1] func04_ret_code=0
         if (func04_ret_code == _OK) {
-            const ret_str = outArg_CharBuffer.readCString();
-            console.log(`[frida] outArg_CharBuffer=[${ret_str}]`);
-            // [frida] outArg_CharBuffer=[name:Zhangsan,id:920,pi:3.141593;zzzzzzzzzzzzz,hex:63,job_cnt:5,msg:hello_world,]
-            //  结果正确
+            const ret_cstr = outArg_CharBuffer.readCString();
+            if (ret_cstr) {
+                console.log(`[调用1] [frida] outArg_CharBuffer=[${ret_cstr}], ret_str.length=${ret_cstr.length}`);
+                // [调用1] [frida] outArg_CharBuffer=[name:Zhangsan,id:4.000000,pi:17;zzzzzzzzzzzzz,hex:63,job_cnt:5,msg:hello_world,], ret_str.length=79
+                //  结果正确
+            }
         }
-    }
-}
+        //第2次调用
+        func04_ret_code = nativeFn__func04_retVoid_outArgCharBuffer(-90909.2, -70070, outArg_CharBuffer);
+        console.log(`[调用2] func04_ret_code=${func04_ret_code}`);
+        // [调用2] func04_ret_code=0
+        if (func04_ret_code == _OK) {
+            const ret_cstr = outArg_CharBuffer.readCString();
+            if (ret_cstr) {
+                console.log(`[调用2] [frida] outArg_CharBuffer=[${ret_cstr}], ret_str.length=${ret_cstr.length}`);
+                // [调用2] [frida] outArg_CharBuffer=[name:Zhangsan,id:-90909.200000,pi:-70070;zzzzzzzzzzzzz,hex:63,job_cnt:5,msg:hello_world,], ret_str.length=88
+                //  结果正确
+            }
+        }
+    } //end of if
+} //end of OnFnLeaveBusz
 function _main_() {
     //获取本地函数func01_return_int
     const func01_return_int = DebugSymbol.fromName("func01_return_int").address;
