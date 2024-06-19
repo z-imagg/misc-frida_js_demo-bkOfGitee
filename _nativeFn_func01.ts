19c8d9bc543ead46f2da2f6d4f4b7184b565433d
prgrmz07 <prgrmz07@163.com>
2024-06-19 09:53:33 +0800

    全局变量前缀g_ , 全局类型前缀G_

--- _nativeFn_func01.ts
+++ _nativeFn_func01.ts
@@ -3,9 +3,9 @@
 
 //frida中表达 函数 func01_return_int 的签名
 // int func01_return_int(char ch, double real_num);
-type FnType_func01 = (ch: number, real_num: number) => number;
+type G_FnType_func01 = (ch: number, real_num: number) => number;
 //持有本地函数
-let nativeFn__func01_return_int:FnType_func01  |null;  
+let g_nativeFn__func01_return_int:G_FnType_func01  |null;  
 
 
 
@@ -13,8 +13,8 @@ let nativeFn__func01_return_int:FnType_func01  |null;
 function demo_call_nativeFn_func01( ){
 
   //调用本地函数 func01_return_int
-  if(nativeFn__func01_return_int){
-    const ret_int:number=nativeFn__func01_return_int(32,-33); //结果应该是-9
+  if(g_nativeFn__func01_return_int){
+    const ret_int:number=g_nativeFn__func01_return_int(32,-33); //结果应该是-9
     logWriteLn(`[nativeFn__func01_return_int],ret_int=[${ret_int}]`)
   } 
 
@@ -25,7 +25,7 @@ function get__func01_return_int(){
 
   //获取本地函数func01_return_int
   const func01_return_int:NativePointer = DebugSymbol.fromName("func01_return_int").address;
-  nativeFn__func01_return_int=  new NativeFunction(func01_return_int, 'int',['char','double']);
-  logWriteLn(`##nativeFn__func01_return_int=${nativeFn__func01_return_int}`)
+  g_nativeFn__func01_return_int=  new NativeFunction(func01_return_int, 'int',['char','double']);
+  logWriteLn(`##nativeFn__func01_return_int=${g_nativeFn__func01_return_int}`)
 
 }
