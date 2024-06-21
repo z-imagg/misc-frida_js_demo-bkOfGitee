62c906b35a852e2549c8946e31342fec105c78f1
prgrmz07 <prgrmz07@163.com>
2024-06-21 23:49:04 +0800

    [结论] frida在onEnter后、onLeave前  销毁了args, args能在onEnter内用, args不能在onLeave内用
     在onEnter内时将args放到thiz下, 等到onLeave内时使用thiz.args收到报错
     报错为 'Error: invalid operation'

--- InterceptFnSym.ts
+++ InterceptFnSym.ts
@@ -43,8 +43,14 @@ function OnFnEnterBusz(thiz:InvocationContext,  args:InvocationArguments){
   const fnAdr:NativePointer=thiz.context.pc;
   const fnSym :DebugSymbol|undefined= findFnDbgSym(fnAdr)
   logWriteLn(`[OnFnEnterBusz],fnSym=[${fnSym}]`)
+  thiz.fnAdr_OnFnEnterBusz=fnAdr;
 
+  //对函数func05_userQuery的特殊处理
   if(fnSym && fnSym.name=="func05_userQuery"){
+    //把参数们args保存到thiz下, 试图供给函数离开时候使用。 但 实践表明， args能在onEnter内用, args不能在onLeave内用 。
+    thiz.args=args;
+    console.log(`[frida_js OnFnEnterBusz] json(thiz)=[${JSON.stringify(thiz)}]`);
+
 // func05_userQuery 函数签名
 // /fridaAnlzAp/frida_js_demo/app.c
 // float func05_userQuery(char sex, int userId, int userName_limit, char* userName_out_, int* userName_length_out_);
@@ -55,26 +61,25 @@ function OnFnEnterBusz(thiz:InvocationContext,  args:InvocationArguments){
 
     const arg0_toInt32:number=args[0].toInt32() // ==  sex
     const arg0_toInt32_toAscii:string=to_ascii(arg0_toInt32)
-    console.log(`[frida_js] arg0_toInt32=[${arg0_toInt32}], arg0_toInt32_toAscii=[${arg0_toInt32_toAscii}]`);
+    console.log(`[frida_js OnFnEnterBusz] arg0_toInt32=[${arg0_toInt32}], arg0_toInt32_toAscii=[${arg0_toInt32_toAscii}]`);
     args[0]=new NativePointer(get_ascii("a"));// 修改 输入参数 sex 为 'a'
 
     const arg1_toInt32:number=args[1].toInt32() // == userId
-    console.log(`[frida_js] arg1_toInt32=[${arg1_toInt32}]`);
+    console.log(`[frida_js OnFnEnterBusz] arg1_toInt32=[${arg1_toInt32}]`);
     args[1]=new NativePointer(-333);// 修改 输入参数 userId 为 -333
 
     const arg2_toInt32:number=args[2].toInt32() // == userName_limit
-    console.log(`[frida_js] arg2_toInt32=[${arg2_toInt32}]`); 
+    console.log(`[frida_js OnFnEnterBusz] arg2_toInt32=[${arg2_toInt32}]`); 
     args[2]=new NativePointer(32);// 修改 输入参数 userName_limit 为 32
     
     const arg3_readCString:string| null=args[3].readCString() // == userName_out_
     if(arg3_readCString){
-      console.log(`[frida_js] arg3_readCString=[${arg3_readCString}]`);
+      console.log(`[frida_js OnFnEnterBusz] arg3_readCString=[${arg3_readCString}]`);
     }
     
     const arg4_readInt:number=args[4].readInt() // == userName_length_out_
-    console.log(`[frida_js] arg4_readInt=[${arg4_readInt}]`);
+    console.log(`[frida_js OnFnEnterBusz] arg4_readInt=[${arg4_readInt}]`);
   }
-  thiz.fnAdr_OnFnEnterBusz=fnAdr;
 
 
 
@@ -92,13 +97,11 @@ function OnFnEnterBusz(thiz:InvocationContext,  args:InvocationArguments){
 // 以命令MyTsCmd导入文件 _nativeFn_func04.ts
 //MyTsCmd//_replaceCurLineByTsFileContent("./_nativeFn_func04.ts" , curNextLn)
 
-
-
 const M_ascii:number='M'.charCodeAt(0);
 
 /**  OnLeave ，函数离开
  */
-function OnFnLeaveBusz(thiz:InvocationContext,  retval:any ){
+function OnFnLeaveBusz(thiz:InvocationContext,  retval:InvocationReturnValue ){
   const curThreadId:ThreadId=Process.getCurrentThreadId()
   const fnAdr:NativePointer=thiz.context.pc;
   const fnSym :DebugSymbol|undefined= findFnDbgSym(fnAdr)
@@ -115,6 +118,41 @@ function OnFnLeaveBusz(thiz:InvocationContext,  retval:any ){
 
   //调用本地函数 func04_retVoid_outArgCharBuffer
   // demo_call_nativeFn_func04(  );
+
+    //对函数func05_userQuery的特殊处理
+  if(fnSym && fnSym.name=="func05_userQuery"){
+    //现在是函数离开时, 由于函数进入时 参数们args被保存在thiz下, 因此此时可以拿出来
+    const args:InvocationArguments=thiz.args
+    console.log(`[frida_js OnFnLeaveBusz] json(thiz)=[${JSON.stringify(thiz)}]`);
+    // func05_userQuery 函数签名
+    // /fridaAnlzAp/frida_js_demo/app.c
+    // float func05_userQuery(char sex, int userId, int userName_limit, char* userName_out_, int* userName_length_out_);
+    
+    //报错,  'Error: invalid operation'
+    const arg0_toInt32:number=args[0].toInt32() // ==  sex
+    const arg0_toInt32_toAscii:string=to_ascii(arg0_toInt32)
+    console.log(`[frida_js OnFnLeaveBusz] arg0_toInt32=[${arg0_toInt32}], arg0_toInt32_toAscii=[${arg0_toInt32_toAscii}]`);
+
+    //报错,  'Error: invalid operation'
+    const arg1_toInt32:number=args[1].toInt32() // == userId
+    console.log(`[frida_js OnFnLeaveBusz] arg1_toInt32=[${arg1_toInt32}]`);
+
+    //报错,  'Error: invalid operation'
+    const arg2_toInt32:number=args[2].toInt32() // == userName_limit
+    console.log(`[frida_js OnFnLeaveBusz] arg2_toInt32=[${arg2_toInt32}]`); 
+    
+    //函数离开时, 获取到 函数出参 userName_out_
+    //报错,  'Error: invalid operation'
+    const arg3_readCString:string| null=args[3].readCString() // == userName_out_
+    if(arg3_readCString){
+      console.log(`[frida_js OnFnLeaveBusz] arg3_readCString=[${arg3_readCString}]`);
+    }
+    
+    //函数离开时, 获取到 函数出参 userName_length_out_
+    //报错,  'Error: invalid operation'
+    const arg4_readInt:number=args[4].readInt() // == userName_length_out_
+    console.log(`[frida_js OnFnLeaveBusz] arg4_readInt=[${arg4_readInt}]`);
+  }
 
 }//end of OnFnLeaveBusz
 
