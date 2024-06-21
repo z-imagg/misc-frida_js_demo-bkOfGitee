c719f73ef6f0058bc91830956225db2531d6abdb
prgrmz07 <prgrmz07@163.com>
2024-06-21 12:15:31 +0800

    [py环境激活 规范化] 使用miniconda3 、所有可执行文件统一用变量表示（不再依靠PATH查找、不再依靠activate）

--- py_envAct_depInstl.sh
+++ py_envAct_depInstl.sh
@@ -4,9 +4,21 @@
 
 cd /fridaAnlzAp/frida_js_demo/
 
-# 激活py环境
-set +x ; source /app/Miniconda3-py310_22.11.1-1/bin/activate ; set -x
-pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple > /dev/null
+_CondaHome=/app/Miniconda3-py310_22.11.1-1
+_CondaBin=$_CondaHome/bin
+_CondaActv=$_CondaBin/activate
+_CondaPip=$_CondaBin/pip
+_CondaPy=$_CondaBin/python
+_CondaFrida=$_CondaBin/frida
+_CondaFridaCompile=$_CondaBin/frida-compile
+
+# 激活py环境(不再需要)
+# source $_CondaActv ; 
+
+#升级pip
+$_CondaPy -m pip install --upgrade pip 
+#pip清华镜像
+$_CondaPip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple > /dev/null
 # py依赖安装
-pip install -r requirements.txt > /dev/null
+$_CondaPip   install -r requirements.txt 
 
