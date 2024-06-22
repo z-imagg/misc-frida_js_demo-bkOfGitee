9b76c2caee221ee586cbb5e74dc5967872c01438
prgrmz07 <prgrmz07@163.com>
2024-06-22 16:40:48 +0800

    添加c++函数  cxxFunc06_outArgString

--- app.cpp
+++ app.cpp
@@ -1,7 +1,18 @@
-#include <stdio.h>
-#include <string.h>
+#include <string>
+
+bool cxxFunc06_outArgString(int num,  std::string * numDescOut_){
+    if(num>=0){
+        numDescOut_->assign("PositiveOrZeroNum");
+    }else{
+        numDescOut_->assign("NegtiveNum");
+    }
+    return true;
+}
 
 
 int main(int argc,char** argv){
+    std::string numDesc;
+    cxxFunc06_outArgString(31, &numDesc);
+    cxxFunc06_outArgString(-120, &numDesc);
     return 0;
 }
